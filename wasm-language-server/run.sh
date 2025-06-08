ee54c5098802c31e52ffdab2c7047e5619a4c936
yportne13 <yportne13@users.noreply.github.com>
2025-06-08 18:46:49 +0000

    deploy: b9fb262e9d967dc49c115a21826a1fb48fc2312f

--- wasm-language-server/run.sh
+++ wasm-language-server/run.sh
@@ -8,7 +8,7 @@ rm -rf sample/wasm-language-server
 mv sample/wasm-language-server-bak/extension sample/wasm-language-server
 mv sample/wasm-language-server-bak/extension.vsixmanifest sample/wasm-language-server/.vsixmanifest
 rm -rf sample/wasm-language-server-bak
-curl -o wasm-wasi-core-1.0.2.vsix https://marketplace.visualstudio.com/_apis/public/gallery/publishers/my-vscode/vsextensions/wasm-wasi-core/1.0.2/vspackage
+curl -o wasm-wasi-core-1.0.2.vsix https://marketplace.visualstudio.com/_apis/public/gallery/publishers/ms-vscode/vsextensions/wasm-wasi-core/1.0.2/vspackage
 unzip wasm-wasi-core-1.0.2.vsix -d sample/wasm-wasi-core-bak
 rm -rf sample/wasm-wasi-core
 mv sample/wasm-wasi-core-bak/extension sample/wasm-wasi-core
