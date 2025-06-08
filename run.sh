1f353c38f566522c7eb2602eef77a589836bc2da
yportne <yportne@163.com>
2025-06-09 01:46:00 +0800

    ci(small fix): fix

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ mv sample/wasm-language-server-bak/extension sample/wasm-language-server
 mv sample/wasm-language-server-bak/extension.vsixmanifest sample/wasm-language-server/.vsixmanifest
 rm -rf sample/wasm-language-server-bak
 curl -o wasm-wasi-core-1.0.2.vsix https://marketplace.visualstudio.com/_apis/public/gallery/publishers/my-vscode/vsextensions/wasm-wasi-core/1.0.2/vspackage
-unzip ms-vscode.wasm-wasi-core-1.0.2.vsix -d sample/wasm-wasi-core-bak
+unzip wasm-wasi-core-1.0.2.vsix -d sample/wasm-wasi-core-bak
 rm -rf sample/wasm-wasi-core
 mv sample/wasm-wasi-core-bak/extension sample/wasm-wasi-core
 mv sample/wasm-wasi-core-bak/extension.vsixmanifest sample/wasm-wasi-core/.vsixmanifest
