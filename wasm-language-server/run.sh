8d32c2b3264569d5ebaac0a8c0069820fd0a000a
yportne13 <yportne13@users.noreply.github.com>
2025-06-08 18:20:47 +0000

    deploy: 04ce4c4643c65a93912791e2276f2ce7e97ae713

---
+++ wasm-language-server/run.sh
@@ -0,0 +1,21 @@
+
+npm install
+npm install -g vsce
+npm run esbuild
+vsce package
+unzip wasm-language-server-1.0.0.vsix -d sample/wasm-language-server-bak
+rm -rf sample/wasm-language-server
+mv sample/wasm-language-server-bak/extension sample/wasm-language-server
+mv sample/wasm-language-server-bak/extension.vsixmanifest sample/wasm-language-server/.vsixmanifest
+rm -rf sample/wasm-language-server-bak
+curl -o wasm-wasi-core-1.0.2.vsix https://marketplace.visualstudio.com/_apis/public/gallery/publishers/my-vscode/vsextensions/wasm-wasi-core/1.0.2/vspackage
+unzip wasm-wasi-core-1.0.2.vsix -d sample/wasm-wasi-core-bak
+rm -rf sample/wasm-wasi-core
+mv sample/wasm-wasi-core-bak/extension sample/wasm-wasi-core
+mv sample/wasm-wasi-core-bak/extension.vsixmanifest sample/wasm-wasi-core/.vsixmanifest
+rm -rf sample/wasm-wasi-core-bak
+curl -o vim.vsix https://marketplace.visualstudio.com/_apis/public/gallery/publishers/vscodevim/vsextensions/vim/1.30.0/vspackage
+unzip vim.vsix -d sample/vim-bak
+mv sample/vim-bak/extension sample/vim
+mv sample/vim-bak/extension.vsixmanifest sample/vim/.vsixmanifest
+rm -rf sample/vim-bak
