f4aa439eb3fe5852429c61714a965468bd738d29
yportne13 <yportne13@users.noreply.github.com>
2026-05-18 09:10:42 +0000

    deploy: 813801724a81068448b92e4e1da5f53356cb8fa3

---
+++ TyportHDL/client/out/extension.js
@@ -0,0 +1,76 @@
+"use strict";
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deactivate = exports.activate = void 0;
+const vscode_1 = require("vscode");
+const vscode_languageclient_1 = require("vscode-languageclient");
+const wasm_wasi_1 = require("@vscode/wasm-wasi");
+const wasm_wasi_lsp_1 = require("@vscode/wasm-wasi-lsp");
+let client;
+let channel;
+async function startLanguageServer(context, wasm) {
+    if (!channel) {
+        channel = vscode_1.window.createOutputChannel('TyportHDL Language Server');
+    }
+    const serverOptions = async () => {
+        const options = {
+            stdio: (0, wasm_wasi_lsp_1.createStdioOptions)(),
+            mountPoints: [
+                { kind: 'workspaceFolder' },
+            ]
+        };
+        const filename = vscode_1.Uri.joinPath(context.extensionUri, 'client', 'server.wasm');
+        const bits = await vscode_1.workspace.fs.readFile(filename);
+        const module = await WebAssembly.compile(bits);
+        const process = await wasm.createProcess('lsp-server', module, { initial: 320, maximum: 16000, shared: true }, options);
+        const decoder = new TextDecoder('utf-8');
+        process.stderr.onData((data) => {
+            channel.append(decoder.decode(data));
+        });
+        return (0, wasm_wasi_lsp_1.startServer)(process);
+    };
+    const clientOptions = {
+        documentSelector: [{ language: "typort" }],
+        outputChannel: channel,
+        synchronize: {
+            fileEvents: vscode_1.workspace.createFileSystemWatcher("**/.clientrc"),
+        },
+        uriConverters: (0, wasm_wasi_lsp_1.createUriConverters)(),
+    };
+    const newClient = new vscode_languageclient_1.LanguageClient('lspClient', 'LSP Client', serverOptions, clientOptions);
+    try {
+        await newClient.start();
+    }
+    catch (error) {
+        newClient.error(`Start failed`, error, 'force');
+    }
+    return newClient;
+}
+async function activate(context) {
+    const wasm = await wasm_wasi_1.Wasm.load();
+    client = await startLanguageServer(context, wasm);
+    const CountFilesRequest = new vscode_languageclient_1.RequestType('wasm-language-server/countFiles');
+    context.subscriptions.push(vscode_1.commands.registerCommand('vscode-samples.wasm-language-server.countFiles', async () => {
+        const folder = vscode_1.workspace.workspaceFolders[0].uri;
+        const result = await client.sendRequest(CountFilesRequest, { folder: client.code2ProtocolConverter.asUri(folder) });
+        vscode_1.window.showInformationMessage(`The workspace contains ${result} files.`);
+    }));
+    context.subscriptions.push(vscode_1.commands.registerCommand('typort-hdl.restartLanguageServer', async () => {
+        if (client) {
+            await client.stop();
+        }
+        client = await startLanguageServer(context, wasm);
+        vscode_1.window.showInformationMessage('TyportHDL Language Server restarted.');
+    }));
+}
+exports.activate = activate;
+function deactivate() {
+    if (client) {
+        return client.stop();
+    }
+}
+exports.deactivate = deactivate;
+//# sourceMappingURL=extension.js.map
