9a2f12725436a50d95e43bb7fdf77a342add292b
yportne13 <yportne13@users.noreply.github.com>
2026-05-19 12:58:27 +0000

    deploy: 29bd26175be1ba6c17e1d9941cf06b54dc9ff2a4

--- depend/vscode-web/dist/out/vs/base/common/worker/simpleWorker.nls.js
+++
@@ -1,5 +0,0 @@
-"use strict";/*!--------------------------------------------------------
- * Copyright (C) Microsoft Corporation. All rights reserved.
- *--------------------------------------------------------*/define("vs/base/common/worker/simpleWorker.nls",{"vs/base/common/platform":["_"]});
-
-//# sourceMappingURL=https://main.vscode-cdn.net/sourcemaps/f1e16e1e6214d7c44d078b1f0607b2388f29d729/core/vs/base/common/worker/simpleWorker.nls.js.map
